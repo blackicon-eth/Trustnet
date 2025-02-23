216c91eab3540c7d1aba176e9d07000bc732dc0c
Blackicon <mattiaverdecchi@yahoo.it>
2025-02-23 13:38:07 -0700

    Removed world env vars

--- lib/env.ts
+++ lib/env.ts
@@ -4,19 +4,12 @@ import { z } from "zod";
 // https://env.t3.gg/docs/nextjs
 export const env = createEnv({
   server: {
-    APP_ID: z.string().min(1),
-    DEV_PORTAL_API_KEY: z.string().min(1),
-    WLD_CLIENT_ID: z.string().min(1),
-    WLD_CLIENT_SECRET: z.string().min(1),
     NEYNAR_API_KEY: z.string().min(1),
     JWT_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_URL: z.string().min(1),
-    NEXT_PUBLIC_APP_ENV: z
-      .enum(["development", "production"])
-      .optional()
-      .default("development"),
+    NEXT_PUBLIC_APP_ENV: z.enum(["development", "production"]).optional().default("development"),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
